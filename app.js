da623fa5620807f8984751681160ea2ed05b1ce3
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-09 13:46:05 -0800

    Update

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //Required data from JSON file 
 const { projects } = require('./data.json');
 
