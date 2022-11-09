e07be1baa44356022a6f463a1b80b2f524bd1666
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-08 20:10:44 -0800

    Update

--- app.js
+++ app.js
@@ -2,9 +2,8 @@
 const express = require('express');
 const app = express();
 
-//Require data from JSON file 
-const data = require('./data.json');
-const { projects } = data;
+//Required data from JSON file 
+const { projects } = require('./data.json');
 
 //Setting up middleware
 app.set('view engine', 'pug');
