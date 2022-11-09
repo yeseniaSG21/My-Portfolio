801d8bd2334071c6fa97ae450e157275ebb52c03
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-08 21:12:14 -0800

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ const { projects } = require('./data.json');
 
 //Setting up middleware
 app.set('view engine', 'pug');
+
+
+//Set up local server and print a message to terminal
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000! It works!!');
+});
