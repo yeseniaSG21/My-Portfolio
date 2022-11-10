12c2e3d0ddd2f92351684f7dff529be6dd46399e
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-11-09 21:53:02 -0800

    Update

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const path = require("path");
 
-const router = express.Router();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
